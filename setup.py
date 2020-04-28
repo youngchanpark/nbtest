da6931ff93e456393dbcebee7a5f3f5efc19df62
Young-Chan Park <young.chan.park93@gmail.com>
2020-04-28 22:43:09 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with io.open("nbtest/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setup(
+    name = "nbtest",
+    version = version,
+    install_requires = [],
+    include_package_data = False,
+    
+# Metadata
+    author = "Young-Chan Park",
+    author_email = "young.chan.park93@gmail.com",
+    description = "",
+    packages = find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+)
