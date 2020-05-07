2a3f8c79869f8b3cb79bd4f97ae1a2599c85c8a8
Young-Chan Park <young.chan.park93@gmail.com>
2020-05-07 22:09:36 +0900

    Prettified the output message a bit

--- src/testmynb/handler.py
+++ src/testmynb/handler.py
@@ -4,67 +4,106 @@ import sys
 
 from . import __version__ as testmynb__version__
 
+
+def green(text):
+    return f"\033[38;5;82m{text}\033[0m"
+
+def red(text):
+    return f"\033[38;5;197m{text}\033[0m"
+
+def orange(text):
+    return f"\033[38;5;214m{text}\033[0m"
+
 class TestHandler:
     def __init__(self, *notebooks):
         self.notebooks = notebooks
-            
-    def __call__(self):
-        
-        for nb in self.notebooks:
-            nb()
-        
+
+
+    @property
+    def _summary(self):
         notebook_count = len(self.notebooks)
         test_count = sum([len(nb.extract_codes()) for nb in self.notebooks])
         py_ver = re.sub(r'\s.*', '', sys.version)
-        
-        head_message = f' Test My Notebook ({testmynb__version__}) '
-        col, _ = os.get_terminal_size()
-        num_equals = (col - len(head_message)) // 2
-        equals_sign = num_equals * '='
-
-        print(
-            f'{equals_sign}{head_message}{equals_sign}\n'
+        header = self._h1_message(f'Test My Notebook ({testmynb__version__})')
+        return (
+            f'{header}'
             f'Platform {sys.platform}\n'
             f'Python {py_ver}\n'
             f'Working Directory: {os.getcwd()}\n'
             '\n'
             f'{test_count} test cells across {notebook_count} notebook(s) detected.\n'
-            '\n'
-            'Notebooks:'
-            ''
         )
+
+
+    @staticmethod
+    def _h1_message(message):
+        col, _ = os.get_terminal_size()
+        num_equals = (col - len(message) - 3) // 2
+        equals_sign = num_equals * '='
+        
+        return f'{equals_sign} {message} {equals_sign}\n'
+    
+
+    @property
+    def _notebook_summary_section(self):
+        section = ['Notebooks:\n']
         for nb in self.notebooks:
-            trust = 'Trusted' if nb.trusted else 'Untrusted'
-            print(f'{trust} {nb.name}: {nb.result}')
-        print('\n')
+            trust = green("Trusted") if nb.trusted else red('Untrusted')
+            section.append(f'{trust} {nb.name}: {nb.result}\n')
+        section.append('\n')
+        return ''.join(section)
+
+
+    def __call__(self):
+        
+        for nb in self.notebooks:
+            nb()
+        
+        print(self._summary)
+        print(self._notebook_summary_section)
+
+
         errors = self.collect_errors()
         fails = self.collect_fails()
-        
-        if errors:
-            head_message = ' Errored Test(s) '
-            num_equals = (col - len(head_message)) // 2
-            equals_sign = num_equals * '='
-            print(f'{equals_sign}{head_message}{equals_sign}\n')
-            for cell, err in errors.items():
-                print(f'---- {cell.notebook}: {cell.name} ----\n')
-                print(cell)
-                print('-----------------------------------------')
-                print(err)
-                #print(f'---- {cell.notebook}: {cell.name} ----\n')
-                print('\n\n\n\n\n')
-                
+
         if fails:
-            head_message = ' Failed Test(s) '
-            num_equals = (col - len(head_message)) // 2
-            equals_sign = num_equals * '='
-            print(f'{equals_sign}{head_message}{equals_sign}\n')
+            head_message = red(self._h1_message('Failed Test(s)'))
+            print(head_message)
             for cell, err in fails.items():
                 print(f'---- {cell.notebook}: {cell.name} ----\n')
                 print(cell)
-                print('-----------------------------------------')
+                print(red('-----------------------------------------'))
                 print(err)
-                #print(f'---- {cell.notebook}: {cell.name} ----\n') 
-                print('\n\n\n\n\n')
+                print('\n\n')
+
+
+        if errors:
+            head_message = orange(self._h1_message('Errored Test(s)'))
+            print(head_message)
+            for cell, err in errors.items():
+                print(f'---- {cell.notebook}: {cell.name} ----\n')
+                print(cell)
+                print(red('-----------------------------------------'))
+                print(err)
+                print('\n\n')
+
+        print(self._final_remarks)
+                
+
+
+
+    @property
+    def _final_remarks(self):
+        all_tests = ''.join([nb.result for nb in self.notebooks])
+
+        passed_test_count = all_tests.count('.')
+        failed_test_count = all_tests.count('F')
+        errored_test_count = all_tests.count('E')
+        
+        f'{green(f"{passed_test_count} test(s) passed")}, {red(f"{failed_test_count} failed")},{orange(f" and {errored_test_count} raised an error")}'
+
+        return self._h1_message(f'{green(f"{passed_test_count} test(s) passed")}, {red(f"{failed_test_count} failed")},{orange(f" and {errored_test_count} raised an error")}')
+    
 
     def collect_errors(self):
         errors = dict()
