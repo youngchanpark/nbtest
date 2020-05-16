2de09c61942f693ee7f8db6ac998b14702e80580
Young-Chan Park <young.chan.park93@gmail.com>
2020-05-16 15:10:38 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import io
 import re
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 with io.open("src/testmynb/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
@@ -11,13 +13,27 @@ setup(
     version=version,
     install_requires=["nbformat>=5.0.0", "ipython", "click", "pytest"],
     include_package_data=False,
+    entry_points={"console_scripts": ["testmynb = testmynb.main:cli",],},
     # Metadata
     author="Young-Chan Park",
     author_email="young.chan.park93@gmail.com",
-    description="",
+    description="A Jupyter Notebook Testing Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    classifiers=["Programming Language :: Python :: 3"],
-    entry_points={"console_scripts": ["testmynb = testmynb.main:cli",],},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
     python_requires=">=3.5.3",
 )
